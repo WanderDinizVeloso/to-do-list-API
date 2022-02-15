bef076f0875a152aeb55b45a38245b632677acac
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-15 15:59:21 -0300

    implementa root no app

--- src/api/app.js
+++ src/api/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const { json } = require('body-parser');
 require('dotenv').config();
 
+const root = require('../controllers/routers/root');
+
 const app = express();
 
 app.use(json());
+app.use(root);
 
 module.exports = app;
