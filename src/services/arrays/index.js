0f12495a88abaff61153e4fcb02760ff942f1caf
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-16 15:37:24 -0300

    cria sortOrder e sortBy no services arrays

--- src/services/arrays/index.js
+++ src/services/arrays/index.js
@@ -2,6 +2,16 @@ const toDoStatus = [
   'editável', 'pendente', 'em andamento', 'pronto',
 ];
 
+const sortOrder = [
+  'DESC', 'ASC',
+];
+
+const sortBy = [
+  'message', 'updateAt', 'status', 'createdAt',
+];
+
 module.exports = {
   toDoStatus,
+  sortOrder,
+  sortBy,
 };
