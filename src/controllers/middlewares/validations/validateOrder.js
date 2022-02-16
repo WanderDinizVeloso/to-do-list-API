2ad795a08c7e0511906b87ded300ad533b52db4f
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-16 17:27:20 -0300

    cria middleware validateOrder

---
+++ src/controllers/middlewares/validations/validateOrder.js
@@ -0,0 +1,16 @@
+const { paramWithListVerify } = require('../../../services/validations');
+const { invalidParamWithList } = require('../../statusAndMessage');
+const { ORDER } = require('../../../services/strings');
+const { sortOrder } = require('../../../services/arrays');
+
+module.exports = async (req, _res, next) => {
+  const { order } = req.query;
+
+  const verifiedOrder = paramWithListVerify(order, sortOrder);
+
+  if (!verifiedOrder) {
+    return next(invalidParamWithList(ORDER, sortOrder));
+  }
+
+  return next();
+};
