8f49c5234b36d707067cd260f14e3d0c35ffa6e1
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-15 23:06:18 -0300

    cria controllers users update

---
+++ src/controllers/documents/users/update.js
@@ -0,0 +1,23 @@
+const { OK } = require('http-status-codes').StatusCodes;
+
+const { update } = require('../../../services/documents/users');
+const { modifiedSuccessfully, notFound } = require('../../statusAndMessage');
+const { USER } = require('../../../services/strings');
+
+module.exports = async (req, res, next) => {
+  const { id } = req.users;
+  const { firstName, lastName, email, password } = req.body;
+
+  const updated = await update({ id, firstName, lastName, email, password });
+
+  if (!updated) {
+    return next(notFound(USER));
+  }
+
+  return res
+    .status(OK)
+    .json({
+      message: modifiedSuccessfully(USER),
+      updatedUser: updated,
+    });
+};
