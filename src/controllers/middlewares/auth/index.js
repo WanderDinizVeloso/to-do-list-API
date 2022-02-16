e9bdfc9cf710f082d742ae805d745164cda3ebc3
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-16 00:23:32 -0300

    implementa async/await no middleware ahth

--- src/controllers/middlewares/auth/index.js
+++ src/controllers/middlewares/auth/index.js
@@ -2,14 +2,14 @@ const { verifyToken } = require('../../../services/auth');
 const { invalid, notFound } = require('../../statusAndMessage');
 const { TOKEN } = require('../../../services/strings');
 
-module.exports = (req, _res, next) => {
+module.exports = async (req, _res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization) {
     return next(notFound(TOKEN));
   }
 
-  const user = verifyToken(authorization);
+  const user = await verifyToken(authorization);
 
   if (!user) {
     return next(invalid(TOKEN));
