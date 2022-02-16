10f3e415e1b11dd7d793d794fabecde0ad85d901
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-16 18:01:30 -0300

    implementa middlewares de validacao no router toDos

--- src/controllers/routers/toDos.js
+++ src/controllers/routers/toDos.js
@@ -1,28 +1,37 @@
 const express = require('express');
 
-const { wrapper, auth } = require('../middlewares');
-
+const {
+  wrapper, auth, validateMessage, validateStatus, validateId, validateOrder, validateSort,
+} = require('../middlewares');
 const { create, remove, searchAll, update } = require('../documents/toDos');
 
 const router = express.Router({ mergeParams: true });
 
 router.get('/', wrapper([
   auth,
+  validateSort,
+  validateOrder,
   searchAll,
 ]));
 
 router.post('/', wrapper([
   auth,
+  validateMessage,
+  validateStatus,
   create,
 ]));
 
 router.put('/:id', wrapper([
   auth,
+  validateId,
+  validateMessage,
+  validateStatus,
   update,
 ]));
 
 router.delete('/:id', wrapper([
   auth,
+  validateId,
   remove,
 ]));
 
