ffada7e50d1f0869a601b45817db98f8ef06ed7a
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-16 08:13:26 -0300

    cria index para services toDos

---
+++ src/services/documents/toDos/index.js
@@ -0,0 +1,11 @@
+const create = require('./create');
+const remove = require('./remove');
+const searchById = require('./searchById');
+const update = require('./update');
+
+module.exports = {
+  create,
+  remove,
+  searchById,
+  update,
+};
