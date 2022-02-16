3713b021b6e4188c96c68f54068daa6217a9df09
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-16 11:29:29 -0300

    implementa ordenacao dos todos via Query Parameters

--- src/services/documents/toDos/searchAll.js
+++ src/services/documents/toDos/searchAll.js
@@ -1,8 +1,12 @@
 const { TO_DOS } = require('../../strings');
 const { searchAll } = require('../../../models')(TO_DOS);
+const { sortSearch } = require('../../functions');
 
-module.exports = async (userId) => {
-  const toDos = await searchAll({ userId });
+module.exports = async (userId, sort = 'createdAt', order = 'ASC') => {
+  const toDos = await searchAll(
+    { userId },
+    sortSearch(sort, order),
+  );
 
   return toDos;
 };
