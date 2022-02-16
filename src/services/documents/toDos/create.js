40e9758475a9f3a912b139b018185916f5fc8f19
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-16 08:09:10 -0300

    refatora services toDos create, para implementacao correta da data de criacao

--- src/services/documents/toDos/create.js
+++ src/services/documents/toDos/create.js
@@ -1,8 +1,10 @@
 const { TO_DOS } = require('../../strings');
 const { create, searchById } = require('../../../models')(TO_DOS);
 
-module.exports = async ({ message, status, date, userId }) => {
-  const { insertedId } = await create({ message, status, date, userId });
+module.exports = async ({ message, status, userId }) => {
+  const date = new Date();
+
+  const { insertedId } = await create({ message, status, createdAt: date, userId });
 
   const createdToDo = await searchById(insertedId);
 
