bf325bc6e754b5af79c2353217865e298ca1d5b1
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-16 16:37:52 -0300

    cria invalidEmail, invalidParamWithList, invalidPassword e invalidString no controllers statusAndMessage

--- src/controllers/statusAndMessage/index.js
+++ src/controllers/statusAndMessage/index.js
@@ -1,5 +1,17 @@
 const { CONFLICT, NOT_FOUND, BAD_REQUEST } = require('http-status-codes').StatusCodes;
 
+const messageItems = {
+  required: () => '- Is required',
+  type: (type) => `- Must be a ${type}`,
+  length: (length) => `- Must contain at least ${length} characters`,
+  format: (format) => `- Must contain the format ${format}.`,
+  containList: (list) => `the status must be at least one items: ${list.toString()}`,
+  passwordAttribute: () => `- must contain at least:
+    - a capital letter;
+    - a number
+    - a special character (!, $, #, %, _).`,
+};
+
 const createdSuccessfully = (param) =>
   `'${param}' created successfully.`;
 
@@ -27,6 +39,39 @@ const invalid = (param) => ({
 const internalError = () =>
   'sorry, internal error.';
 
+const invalidEmail = () => ({
+  status: BAD_REQUEST,
+  message: `The 'email' field must contain at least:
+    ${messageItems.required()}
+    ${messageItems.type('string')}
+    ${messageItems.format('email@email.com')}`,
+});
+
+const invalidPassword = (length) => ({
+  status: BAD_REQUEST,
+  message: `The 'email' field must contain at least:
+    ${messageItems.required()}
+    ${messageItems.type('string')}
+    ${messageItems.length(length)}
+    ${messageItems.passwordAttribute()}`,
+});
+
+const invalidParamWithList = (param, list) => ({
+  status: BAD_REQUEST,
+  message: `The '${param}' field must contain at least:
+    ${messageItems.required()}
+    ${messageItems.type('string')}
+    ${messageItems.containList(list)}`,
+});
+
+const invalidString = (param, length) => ({
+  status: BAD_REQUEST,
+  message: `The '${param}' field must contain at least:
+    ${messageItems.required()}
+    ${messageItems.type('string')}
+    ${messageItems.length(length)}`,
+});
+
 module.exports = {
   createdSuccessfully,  
   registered,
@@ -35,4 +80,8 @@ module.exports = {
   modifiedSuccessfully,
   invalid,
   internalError,
+  invalidEmail,
+  invalidParamWithList,
+  invalidPassword,
+  invalidString,
 };
