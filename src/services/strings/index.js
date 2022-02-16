2978a199c6eb1942a3767a37abf5d8af32b3f95a
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-15 23:57:56 -0300

    cria TOKEN no services strings

--- src/services/strings/index.js
+++ src/services/strings/index.js
@@ -1,11 +1,13 @@
 const USER = 'user';
 const USERS = 'users';
 const EMAIL = 'email';
+const TOKEN = 'token';
 const TOKENS = 'tokens';
 
 module.exports = {
   USER,
   USERS,
   EMAIL,
+  TOKEN,
   TOKENS,
 };
