d46d3d11ee8d2903a0cad0e9545a1e6df3956ab1
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-15 23:49:56 -0300

    implementa verificacao de blackList de tokens para melhor seguranca dos dados de autenticacao

--- src/services/auth/verifyToken.js
+++ src/services/auth/verifyToken.js
@@ -2,9 +2,18 @@ const { verify } = require('jsonwebtoken');
 
 const { SECRET } = process.env;
 
-module.exports = (token) => {
+const { TOKENS } = require('../strings');
+const { searchByField } = require('../../models')(TOKENS);
+
+module.exports = async (token) => {
   try {
     const decoded = verify(token, SECRET);
+
+    const blackListExist = await searchByField({ token });
+
+    if (blackListExist) {
+      return null;
+    }
 
     return decoded.data;
   } catch (err) {
