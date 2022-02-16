e290b6471ebbd47f2139ddd4fcc78f651000a99c
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-16 15:46:42 -0300

    cria index para services validations

---
+++ src/services/validations/index.js
@@ -0,0 +1,13 @@
+const emailVerify = require('./emailVerify');
+const paramWithListVerify = require('./paramWithListVerify');
+const passwordVerify = require('./passwordVerify');
+const statusVerify = require('./statusVerify');
+const stringsVerify = require('./stringsVerify');
+
+module.exports = {
+  emailVerify,
+  paramWithListVerify,
+  passwordVerify,
+  statusVerify,
+  stringsVerify,
+};
