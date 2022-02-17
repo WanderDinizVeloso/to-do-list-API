7e58031e37cd744a2deef8c680571463108451a7
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-16 22:54:06 -0300

    implementa middlewares de validacao no router users

--- src/controllers/routers/users.js
+++ src/controllers/routers/users.js
@@ -1,21 +1,35 @@
 const express = require('express');
 
 const { create, remove, update } = require('../documents/users');
-const { wrapper, auth } = require('../middlewares');
+const {
+  wrapper, auth, validateFirstName, validateLastName, validateEmail, validatePassword,
+} = require('../middlewares');
 
 const router = express.Router({ mergeParams: true });
 
 router.post('/', wrapper([
+  validateFirstName,
+  validateLastName,
+  validateEmail,
+  validatePassword,
   create,
 ]));
 
 router.put('/', wrapper([
   auth,
+  validateFirstName,
+  validateLastName,
+  validateEmail,
+  validatePassword,
   update,
 ]));
 
 router.delete('/', wrapper([
   auth,
+  validateFirstName,
+  validateLastName,
+  validateEmail,
+  validatePassword,
   remove,
 ]));
 
