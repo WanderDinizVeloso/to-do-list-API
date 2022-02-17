3c9eda3373310f0f01487548609a22373cdd21ae
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-16 23:24:09 -0300

    cria idVerify no servicesvalidations

--- src/services/validations/index.js
+++ src/services/validations/index.js
@@ -2,10 +2,12 @@ const emailVerify = require('./emailVerify');
 const paramWithListVerify = require('./paramWithListVerify');
 const passwordVerify = require('./passwordVerify');
 const stringsVerify = require('./stringsVerify');
+const idVerify = require('./idVerify');
 
 module.exports = {
   emailVerify,
   paramWithListVerify,
   passwordVerify,
   stringsVerify,
+  idVerify,
 };
